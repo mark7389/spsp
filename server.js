bb21cb01f8705d600d92f38608f7934f73aa8324
mark <mark.soliman73@gmail.com>
2018-09-08 23:40:43 -0400

    staging

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(bodyParser.json({type:['application/json'],limit:'50mb'}))
 app.use(bodyParser.urlencoded({extended:true,limit:'50mb'}));
 app.use(bodyParser.text({limit:'50mb'}));
 app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Origin','http://192.168.77.100:4200')
+    res.header('Access-Control-Allow-Origin','http://192.168.77.100')
     res.header('Access-Control-Allow-Methods','GET, POST')
     return next()
 });
 
 
 
-app.use(express.static('public'));
+app.use(express.static('dist/spsp-attendance'));
 app.use("/", routes);
 
 
