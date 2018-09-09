80bdb84fa2ba80cd4ebecdaa7b02fd7f642074d9
mark <mark.soliman73@gmail.com>
2018-09-08 23:27:22 -0400

    commit for staging

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const bodyParser = require('body-parser');
 const routes = require("./lib/routes");
 const port = process.env.Port || 3000;
 
+app.use(bodyParser.json({type:['application/json'],limit:'50mb'}))
 app.use(bodyParser.urlencoded({extended:true,limit:'50mb'}));
+app.use(bodyParser.text({limit:'50mb'}));
 app.use(function(req,res,next){
     res.header('Access-Control-Allow-Origin','http://192.168.77.100:4200')
     res.header('Access-Control-Allow-Methods','GET, POST')
     return next()
 });
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.text());
+
 
 app.use(express.static('public'));
 app.use("/", routes);
